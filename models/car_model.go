0092fecb8eb2261cc464ddc7c6831a95e67673cd
adohong4 <adohong4@gmail.com>
2025-07-07 11:13:59 +0700

    feat(models): completing car && engine models

--- models/car_model.go
+++ models/car_model.go
@@ -1,1 +1,116 @@
 package models
+
+import (
+	"errors"
+	"strconv"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Car struct {
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	Year      string    `json:"year"`
+	Brand     string    `json:"brand"`
+	FuelType  string    `json:"fuel_type"`
+	Engine    Engine    `json:"engine"`
+	Price     float64   `json:"price"`
+	CretedAt  time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at`
+}
+
+type CarRequest struct {
+	Name     string  `json:"name"`
+	Year     string  `json:"year"`
+	Brand    string  `json:"brand"`
+	FuelType string  `json:"fuel_type"`
+	Engine   Engine  `json:"engine"`
+	Price    float64 `json:"price"`
+}
+
+func validateRequest(carRequest CarRequest) error {
+	if err := validateName(carRequest.Name); err != nil {
+		return err
+	}
+	if err := validateYear(carRequest.Year); err != nil {
+		return err
+	}
+	if err := validatedBrand(carRequest.Brand); err != nil {
+		return err
+	}
+	if err := ValidateFuelType(carRequest.FuelType); err != nil {
+		return err
+	}
+	if err := valdateEngine(carRequest.Engine); err != nil {
+		return err
+	}
+	if err := validatePrice(carRequest.Price); err != nil {
+		return err
+	}
+	return nil
+}
+
+func validateName(name string) error {
+	if name == "" {
+		return errors.New("Name is Required")
+	}
+	return nil
+}
+
+func validateYear(year string) error {
+	if year == "" {
+		return errors.New("Year is Required")
+	}
+
+	_, err := strconv.Atoi(year)
+	if err != nil {
+		return errors.New("Year must be a valid number")
+	}
+	currentYear := time.Now().Year()
+	yearInt, _ := strconv.Atoi(year)
+	if yearInt < 1886 || yearInt > currentYear {
+		return errors.New("Year must be between 1886 and the current year")
+	}
+	return nil
+}
+
+func validatedBrand(brand string) error {
+	if brand == "" {
+		return errors.New("Brand is Required")
+	}
+	return nil
+}
+
+func ValidateFuelType(fuelType string) error {
+	validFuelTypes := []string{"Petrol", "Diesel", "Electric", "Hybrid"}
+	for _, validFuelType := range validFuelTypes {
+		if fuelType == validFuelType {
+			return nil
+		}
+	}
+	return errors.New("FeulType must be one of Petrol, Diesel, Electric, or Hybrid")
+}
+
+func valdateEngine(engine Engine) error {
+	if engine.EngineID == uuid.Nil {
+		return errors.New("Engine ID is Required")
+	}
+	if engine.Displacement <= 0 {
+		return errors.New("Displacement must be greater than 0")
+	}
+	if engine.NoOfCylinders <= 0 {
+		return errors.New("NoOfCylinders must be greater than 0")
+	}
+	if engine.CarRange <= 0 {
+		return errors.New("CarRange must be greater than 0")
+	}
+	return nil
+}
+
+func validatePrice(price float64) error {
+	if price <= 0 {
+		return errors.New("Price must be greater than 0")
+	}
+	return nil
+}
--- models/engine_model.go
+++ models/engine_model.go
@@ -1,1 +1,54 @@
 package models
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+type Engine struct {
+	EngineID      uuid.UUID `json:"id"`
+	Displacement  int64     `json:"displacement"`
+	NoOfCylinders int64     `json:"noOfCylinders"`
+	CarRange      int64     `json:"carRange"`
+}
+
+type EngineRequest struct {
+	Displacement  int64 `json:"displacement"`
+	NoOfCylinders int64 `json:"noOfCylinders"`
+	CarRange      int64 `json:"carRange"`
+}
+
+func ValidateEngineRequest(EngineReq EngineRequest) error {
+	if err := validateDisplacement(EngineReq.Displacement); err != nil {
+		return err
+	}
+	if err := validateNoOfCylinders(EngineReq.NoOfCylinders); err != nil {
+		return err
+	}
+	if err := validateCarRange(EngineReq.CarRange); err != nil {
+		return err
+	}
+	return nil
+}
+
+func validateDisplacement(displacement int64) error {
+	if displacement <= 0 {
+		return errors.New("Displacement must be greater than 0")
+	}
+	return nil
+}
+
+func validateNoOfCylinders(noOfCylinders int64) error {
+	if noOfCylinders <= 0 {
+		return errors.New("noOfCylinders must be greater than 0")
+	}
+	return nil
+}
+
+func validateCarRange(carRange int64) error {
+	if carRange <= 0 {
+		return errors.New("carRange must be greater than 0")
+	}
+	return nil
+}
